82bbebec5072df6b5641f414ad04896487f82f92
juanm <juanm@LAPTOP-OONN1B0G>
2024-10-15 22:25:39 -0500

    Commit

---
+++ Myconsoleapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Mylibrary;
+
+namespace Myconsoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter mywriter = new MessageWriter();
+            mywriter.Writer("Hello Writer");
+            MessageWriter fwriter = new MessageWriter();
+            fwriter.Writer("Hello Figgle");
+        }
+    }
+}
